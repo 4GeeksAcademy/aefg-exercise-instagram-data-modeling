3c0c115d142af450631f5e2a80ccce531315439c
andres fernandes <andresfernandesg10@gmail.com>
2024-05-02 02:13:47 +0000

    creacion data

--- src/models.py
+++ src/models.py
@@ -7,31 +7,62 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Users(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement = True)
+    name_user = Column(String(250), unique=True, nullable=False)
+    name = Column (String(250))
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+    birthday = Column (Date)
+    biography = Column (string(250))
+    image_user = Column (String(250))
+    
+    def to_dict(self):
+        return {}
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'Post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    quote = Column(String(250))
+    image = Column(String(250), nullable=False)
+    video = Column (String(250),nullable=False)
+    likes = Column(Integer, default = 0)
+    comments = Column(Integer, default = 0)
+
+    def to_dict(self):
+        return {}
+
+class likes(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True, autoincrement = True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable = False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable = False)
+    
+
+    def to_dict(self):
+        return {}
+
+
+
+class comments(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True, autoincrement= True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable= False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    text = Column(string(250))
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+render_er(Base, 'diagram.png')
